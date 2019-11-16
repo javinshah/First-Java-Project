0debb07d73404f6e35c48e52253b861cd924c9ca
javin shah <javinshah66@gmail.com>
2019-11-16 22:06:06 +0530

    My first commit

---
+++ 1/Introduction.java
@@ -0,0 +1,10 @@
+public class Introduction
+{
+    public static void main()
+    
+   {
+       System.out.println("Hello all...My name is Javin Shah and I study in Grade VIII in Ramniwas Bajaj English High School.");
+       System.out.println("I am passionate about playing keyboard and I like to devise and analyze new things in digital technology.");
+       System.out.println("I admire reading books in my leisure time.I show interest in various treks and jeopardies.I am also a spanking jockey.");
+   }
+}
